0d9a7cffded114fe36612b5c6fbd58e560b3496e
guillaume-gomez <guillaume.gomez846@gmail.com>
2020-02-11 00:07:16 +0100

    add constants about validation layers

--- src/constants.rs
+++ src/constants.rs
@@ -1,21 +1,16 @@
 // Constants
-pub const WINDOW_TITLE: &'static str = "00.Base Code";
+pub const WINDOW_TITLE: &'static str = "01";
 pub const WINDOW_WIDTH: u32 = 800;
 pub const WINDOW_HEIGHT: u32 = 600;
+
+use crate::utils::debug::ValidationInfo;
+
+pub const VALIDATION: ValidationInfo = ValidationInfo {
+    is_enable: true,
+    required_validation_layers: ["VK_LAYER_LUNARG_standard_validation"],
+};
 
 use ash::vk_make_version;
 pub const APPLICATION_VERSION: u32 = vk_make_version!(1, 0, 0);
 pub const ENGINE_VERSION: u32 = vk_make_version!(1, 0, 0);
 pub const API_VERSION: u32 = vk_make_version!(1, 0, 92);
-
-use ash::extensions::khr::XlibSurface;
-use ash::extensions::ext::DebugUtils;
-use ash::extensions::khr::Surface;
-
-pub fn required_extension_names() -> Vec<*const i8> {
-    vec![
-        Surface::name().as_ptr(),
-        XlibSurface::name().as_ptr(),
-        DebugUtils::name().as_ptr(),
-    ]
-}
